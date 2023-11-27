3a5b428f096610b630706e25fec87c94160543cc
dabao1955 <Admin@qw5d.onmicrosoft.com>
2023-11-27 13:28:42 +0800

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,7 +2,6 @@
 
 set -eu
 test -d out || mkdir out -p -v
-test -d ../vnd || mkdir vnd -p -v
 test -d ../vendor/mediatek/kernel_modules/gpu/gpu_mali/mali_valhall/mali-r25p0 || cp -r -v ../vendor/mediatek/kernel_modules/gpu/gpu_mali/mali_valhall/mali-r25p0 drivers/gpu/mediatek/gpu_mali/mali_valhall/
 
 BUILD_CROSS_COMPILE=$HOME/aarch64-linux-android-4.9/bin/aarch64-linux-android-
@@ -15,12 +14,12 @@ export ARCH=arm64
 export PATH=${CLANG_PATH}:${PATH}
 
 make -j$(nproc) -C $(pwd) O=$(pwd)/out CROSS_COMPILE=$BUILD_CROSS_COMPILE CLANG_TRIPLE=$CLANG_TRIPLE CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32 \
-    CC=clang \
+    CC="ccache clang" \
     vendor/full/r6p_nodtb_defconfig
 
 A=$(date +%s)
 make -j$(nproc) -C $(pwd) O=$(pwd)/out CROSS_COMPILE=$BUILD_CROSS_COMPILE CLANG_TRIPLE=$CLANG_TRIPLE CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32 \
-    CC=clang \
+    CC="ccache clang" \
     -Werror \
     2>&1 | tee build.txt
 B=$(date +%s)
